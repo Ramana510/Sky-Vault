a1613f59cf7e23deb2da3c1e3732ecc455b1a1d1
Ramana <ramanaamjuri16@gmail.com>
2026-06-20 15:50:35 +0530

    Fixed CORS issue

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ connectDB();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({ origin: process.env.FRONTEND_URL || 'http://localhost:5173', credentials: true }));
+app.use(cors({ origin: process.env.FRONTEND_URL || 'sky-vault-two.vercel.app', credentials: true }));
 
 app.get('/', (req, res) => {
   res.json({ message: 'Welcome to Sky Vault API' });
